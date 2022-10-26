d1d54cff23673c2bff4f45b2e850bd17c8f21298
Nier <1592654136@qq.com>
2022-10-26 17:56:17 +0800

    项目初始化

---
+++ src/main/java/com/example/school_bbs_notice/SchoolBbsNoticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.school_bbs_notice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolBbsNoticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolBbsNoticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/school_bbs_notice/SchoolBbsNoticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.school_bbs_notice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolBbsNoticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
